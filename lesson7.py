d3f88c1aba3c20309f418617404dac5a20137c2b
Polina Vasilyeva <Polinova31@gmail.com>
2025-09-01 16:06:31 +0300

    Add files via upload

--- lesson7.py
+++ lesson7.py
@@ -16,18 +16,18 @@ def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='
 def create_notify_progress(bot):
     def notify_progress(secs_left, chat_id, message_id, total_seconds):
         progbar = render_progressbar(total_seconds, total_seconds - secs_left)
-        if secs_left >= 0:  
-            bot.update_message(
-                chat_id,
-                message_id,
-                f"Осталось: {secs_left} сек\n{progbar}"
-            )
-
-    if secs_left == 0:
-        bot.send_message(chat_id, "Время вышло")
+        bot.update_message(
+            chat_id,
+            message_id,
+             f"Осталось: {secs_left} сек\n{progbar}"
+        )
+        if secs_left == 0:
+            bot.send_message(chat_id, "Время вышло") 
     return notify_progress
 
 def create_try_message(bot):
+    notify_progress = create_notify_progress(bot)
+
     def try_message(chat_id, text):
         seconds = parse(text)
         message_id = bot.send_message(chat_id,"Осталось: {} сек".format(seconds))
@@ -39,12 +39,12 @@ def create_try_message(bot):
             message_id = message_id,
             total_seconds = seconds
         )
-        return try_message
+    return try_message
 
 def main():
     load_dotenv()
     bot = ptbot.Bot(os.getenv('tg_token'))
-    bot.reply_on_message(try_message)
+    bot.reply_on_message(create_try_message(bot))
     bot.run_bot()
 
 
